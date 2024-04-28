b391f927801d6aea40f4228ee971fdf4359e4e90
bourichi taha <bourichi.taha@gmail.com>
2024-04-28 05:45:02 +0100

    courses search with url params categoryId&title

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,8 +1,19 @@
+import { getCategories } from "@/actions/categories"
+import Categories from "@/components/dashboard/search/categories";
+import SearchInput from "@/components/dashboard/search/search-input";
 
 
-const SearchPage = () => {
+const SearchPage = async () => {
+  const categories = await getCategories();
   return (
-    <div>SearchPage</div>
+    <>
+      <div className="px-6 pt-6 md:hidden md:mb-0 ">
+        <SearchInput />
+      </div>
+      <div className="p-6">
+        <Categories items={categories} />
+      </div>
+    </>
   )
 }
 
---
+++ hooks/use-debounce.ts
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from 'react'
+
+export function UseDebounce<T>(value:T,delay?:number): T {
+  
+    const [debounceValue,setDebounceValue] = useState<T>(value);
+
+    useEffect(()=> {
+        const timer = setTimeout(()=> setDebounceValue(value), delay || 500);
+        return ()=>{
+            clearTimeout(timer);
+        }
+    },[value,delay]);
+
+    return debounceValue;
+
+
+}
+
+export default UseDebounce
